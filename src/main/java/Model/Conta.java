b64d18012fa1ed44307738ca40051ed45b311023
Marcus Garcia (FB - Controladoria) <marcusg@fariasbrito.com.br>
2022-05-05 13:11:41 -0300

    criada as classses de modelo

---
+++ src/main/java/Model/Conta.java
@@ -0,0 +1,26 @@
+package Model;
+
+public class Conta {
+	private Long numero;
+	private Double saldo;
+
+	/**
+	 * Gettrs and Settrs da classe Conta
+	 */
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+}
