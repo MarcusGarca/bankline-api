67ca4018a01fc779b5ffefdf504cc8a7fad0a7c9
Marcus Garcia (FB - Controladoria) <marcusg@fariasbrito.com.br>
2022-05-05 14:36:51 -0300

    mapeando as entidades com JPA

--- src/main/java/Model/Movimentacao.java
+++ src/main/java/Model/Movimentacao.java
@@ -2,13 +2,35 @@ package Model;
 
 import java.time.LocalDateTime;
 
-public class Movimentacao {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "tab_movimentacao")
+public class Movimentacao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column(name = "data_hora")
 	private LocalDateTime dataHora;
 	private String descricao;
 	private Double valor;
 	private MovimentacaoTipo tipo;
+
+	@Enumerated(EnumType.STRING)
+	public MovimentacaoTipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(MovimentacaoTipo tipo) {
+		this.tipo = tipo;
+	}
 
 	/**
 	 * Gettrs and Settrs da classe Movimentacao
@@ -43,14 +65,6 @@ public class Movimentacao {
 
 	public void setValor(Double valor) {
 		this.valor = valor;
-	}
-
-	public MovimentacaoTipo getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(MovimentacaoTipo tipo) {
-		this.tipo = tipo;
 	}
 
 }
